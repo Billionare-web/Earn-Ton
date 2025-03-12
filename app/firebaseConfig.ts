a2f19b2b822d36bab6bfec159867b3bcc59b09cf
Mukhammadrasul2011 <msamaritdinov@gmail.com>
2025-03-12 10:10:25 +0500

    changed

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAE4Vej2Aks4Kc2wnS18YX4JqqoDb89NJw",
-    authDomain: "token-e25d1.firebaseapp.com",
-    projectId: "token-e25d1",
-    storageBucket: "token-e25d1.firebasestorage.app",
-    messagingSenderId: "403076692423",
-    appId: "1:403076692423:web:c99d5e74854015356841e2",
+  apiKey: "AIzaSyBpLe1pD-er_U7t5QOnsPYBvF9AfPnl9AQ",
+  authDomain: "earn-ton.firebaseapp.com",
+  projectId: "earn-ton",
+  storageBucket: "earn-ton.firebasestorage.app",
+  messagingSenderId: "1080910098297",
+  appId: "1:1080910098297:web:af556a1a557565e72d49a8",
 };
 
 const app = initializeApp(firebaseConfig);
